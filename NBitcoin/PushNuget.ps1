125245ffeaa90b542738d1f3a1c88cf1a93147c0
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-06-23 18:53:09 +0200

    push tags

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -4,3 +4,4 @@ forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
 (((dir *.nupkg).Name) -match "[0-9]\.[0-9]\.[0-9]\.[0-9]")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
+git push --tags
