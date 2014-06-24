bf85147b3b5715b10941a7c0ad8fc3852b4e3e69
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-06-24 22:40:28 +0200

    missing files

---
+++ NBitcoin.Tests/ColoredCoinsTests.cs
@@ -0,0 +1,33 @@
+﻿using NBitcoin.DataEncoders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NBitcoin.Tests
+{
+	//https://github.com/OpenAssets/open-assets-protocol/blob/master/specification.mediawiki
+	public class ColoredCoinsTests
+	{
+		[Fact]
+		public void CanCreateAssetAddress()
+		{
+			//var key = new Key(TestUtils.ParseHex("18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725"));
+			//var address = key.PubKey.Decompress().GetAddress(Network.Main);
+			//Assert.Equal("16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM", address.ToString());
+
+			//Script script = Script.FromBitcoinAddress(address);
+			//Assert.Equal("OP_DUP OP_HASH160 010966776006953D5567439E5E39F86A0D273BEE OP_EQUALVERIFY OP_CHECKSIG", script.ToString().ToUpper());
+
+			//var o = Encoders.Hex.EncodeData(script.ToRawScript().ToArray());
+
+			//var hash = script.ID;
+
+			//var scriptAddress = script.GetAddress(Network.Main);
+			//Assert.Equal("3AH9zTfTEo7tKi3EYWQuofb1pzWtZJ4EeS", scriptAddress.ToString());
+		}
+
+	}
+}
