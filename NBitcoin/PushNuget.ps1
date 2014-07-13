ab442f81ac8b47bd3222ecfca0a610f312a526e3
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-07-13 11:06:03 +0200

    fix PushNuget.ps1 to correctly push tag in git

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -1,7 +1,7 @@
 del *.nupkg 
 nuGet pack NBitcoin.csproj -Build -Properties Configuration=Release -includereferencedprojects
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
-(((dir *.nupkg).Name) -match "[0-9]\.[0-9]\.[0-9]\.[0-9]")
+(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
