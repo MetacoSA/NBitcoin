6f4d16d086513f48c52f647babfa5f28ba8a9f0c
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-08-05 16:46:42 +0200

    update copyright

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AO-IS")]
 [assembly: AssemblyProduct("NBitcoin")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright © AO-IS 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly:InternalsVisibleTo("NBitcoin.Tests")]
