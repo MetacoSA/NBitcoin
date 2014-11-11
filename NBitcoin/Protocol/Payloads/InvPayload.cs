750dece4e215a67066f67511b08f31ff5af5b339
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-11-11 21:26:54 +0100

    helper ctor for invpayload

--- NBitcoin/Protocol/Payloads/InvPayload.cs
+++ NBitcoin/Protocol/Payloads/InvPayload.cs
@@ -13,6 +13,21 @@ namespace NBitcoin.Protocol
 		{
 
 		}
+		public InvPayload(params Transaction[] transactions)
+			: this(transactions.Select(tx => new InventoryVector(InventoryType.MSG_TX, tx.GetHash())).ToArray())
+		{
+
+		}
+		public InvPayload(params Block[] blocks)
+			: this(blocks.Select(b => new InventoryVector(InventoryType.MSG_BLOCK, b.GetHash())).ToArray())
+		{
+
+		}
+		public InvPayload(InventoryType type, params uint256[] hashes)
+			: this(hashes.Select(h => new InventoryVector(type, h)).ToArray())
+		{
+
+		}
 		public InvPayload(params InventoryVector[] invs)
 		{
 			_Inventory.AddRange(invs);
