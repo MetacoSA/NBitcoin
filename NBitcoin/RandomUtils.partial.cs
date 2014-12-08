6e4751dd49697f0aa624db4266292fb159c72757
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-12-08 16:28:16 +0100

    missing file

---
+++ NBitcoin/RandomUtils.partial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBitcoin
+{
+	public class RNGCryptoServiceProviderRandom : IRandom
+	{
+		readonly RNGCryptoServiceProvider _Instance;
+		public RNGCryptoServiceProviderRandom()
+		{
+			_Instance = new RNGCryptoServiceProvider();
+		}
+		#region IRandom Members
+
+		public void GetBytes(byte[] output)
+		{
+			_Instance.GetBytes(output);
+		}
+
+		#endregion
+	}
+
+	public partial class RandomUtils
+	{
+		static RandomUtils()
+		{
+			//Thread safe http://msdn.microsoft.com/en-us/library/system.security.cryptography.rngcryptoserviceprovider(v=vs.110).aspx
+			Random = new RNGCryptoServiceProviderRandom();
+		}
+	}
+}
