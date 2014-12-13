93097799938ed813085ee336eaff6bd427c4c79a
Nicolas DORIER <nicolas.dorier@gmail.com>
2014-12-13 01:31:16 +0100

    Rename project, update nuget scripts

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -1,8 +1,8 @@
 del *.nupkg
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin\NBitcoin.csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(portable-net45+netcore45+wpa81+MonoAndroid1+MonoTouch1).csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(portable-net45+netcore45+wpa81+wp8+MonoAndroid1+MonoTouch1).csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Android\NBitcoin.Android.csproj" -p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(Profile111).csproj" -p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(Profile259).csproj" -p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Android\NBitcoin(Android).csproj" -p:Configuration=Release
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.csproj"
 
 nuGet pack NBitcoin.nuspec
