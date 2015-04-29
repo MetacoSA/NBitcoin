b4b68fec88f138f5191f8905be290550bfd7d1c7
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-04-29 03:58:04 +0200

    fix hungarian bug #73

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.8.17")]
-[assembly: AssemblyFileVersion("1.0.8.17")]
-[assembly: AssemblyInformationalVersion("1.0.8.17")]
+[assembly: AssemblyVersion("1.0.8.18")]
+[assembly: AssemblyFileVersion("1.0.8.18")]
+[assembly: AssemblyInformationalVersion("1.0.8.18")]
