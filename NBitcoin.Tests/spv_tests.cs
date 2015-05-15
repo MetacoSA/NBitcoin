fe873477e8f061e577b466f7a9185960d685432f
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-05-16 00:51:17 +0200

    can get transactions from the tracker

--- NBitcoin.Tests/spv_tests.cs
+++ NBitcoin.Tests/spv_tests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,18 +18,18 @@ namespace NBitcoin.Tests
 			Blocks = new Dictionary<uint256, Block>();
 		}
 
-		public Coin GiveMoney(IDestination destination, Money value)
+		public Transaction GiveMoney(IDestination destination, Money value)
 		{
 			return GiveMoney(destination.ScriptPubKey, value);
 		}
 
-		public Coin GiveMoney(Script scriptPubKey, Money value)
+		public Transaction GiveMoney(Script scriptPubKey, Money value)
 		{
 			var tx = new Transaction();
 			tx.Outputs.Add(new TxOut(value, scriptPubKey));
 			var h = tx.GetHash();
 			Mempool.Add(h, tx);
-			return tx.Outputs.AsCoins().FirstOrDefault();
+			return tx;
 		}
 
 		public ConcurrentChain Chain
@@ -86,20 +87,92 @@ namespace NBitcoin.Tests
 			Key bob = new Key();
 			Tracker tracker = new Tracker();
 			tracker.Add(bob);
-			var coin = builder.GiveMoney(bob, Money.Coins(1.0m));
-			Assert.True(tracker.NotifyTransaction(builder.Mempool[coin.Outpoint.Hash]));
-
+			var tx1 = builder.GiveMoney(bob, Money.Coins(1.0m));
+			var coin = tx1.Outputs.AsCoins().First();
+			Assert.True(tracker.NotifyTransaction(tx1));
+			Thread.Sleep(10);
 			Key alice = new Key();
-			var tx = builder.SpendCoin(coin, alice, Money.Coins(0.6m));
-			Assert.True(tracker.NotifyTransaction(tx));
+			var tx2 = builder.SpendCoin(coin, alice, Money.Coins(0.6m));
+			Assert.True(tracker.NotifyTransaction(tx2));
 
 			var block = builder.FindBlock();
 
 			foreach(var btx in block.Transactions)
 			{
-				Assert.True(tracker.NotifyTransaction(btx, builder.Chain.GetBlock(btx.GetHash()), block));
-				Assert.True(tracker.NotifyTransaction(btx, builder.Chain.GetBlock(btx.GetHash()), block)); //Idempotent
+				Assert.True(tracker.NotifyTransaction(btx, builder.Chain.GetBlock(block.GetHash()), block));
+				Assert.True(tracker.NotifyTransaction(btx, builder.Chain.GetBlock(block.GetHash()), block)); //Idempotent
 			}
+
+			var transactions = tracker.GetWalletTransactions(builder.Chain);
+
+			Assert.True(transactions.Count == 2);
+			Assert.True(transactions[0].Transaction.GetHash() == tx2.GetHash());
+			Assert.True(transactions[1].Transaction.GetHash() == tx1.GetHash());
+			Assert.True(transactions[0].Balance == -Money.Coins(0.6m));
+
+			var tx3 = builder.GiveMoney(bob, Money.Coins(0.01m));
+			coin = tx3.Outputs.AsCoins().First();
+			block = builder.FindBlock();
+			Assert.True(tracker.NotifyTransaction(block.Transactions[0], builder.Chain.GetBlock(block.GetHash()), block));
+
+			transactions = tracker.GetWalletTransactions(builder.Chain);
+			Assert.True(transactions.Count == 3);
+			Assert.True(transactions[0].Transaction.GetHash() == block.Transactions[0].GetHash());
+
+			Assert.Equal(2, transactions.GetSpendableCoins().Count()); // the 1 change + 1 gift
+
+			builder.Chain.SetTip(builder.Chain.Tip.Previous);
+			transactions = tracker.GetWalletTransactions(builder.Chain);
+			Assert.True(transactions.Count == 2);
+		}
+
+		[Fact]
+		public void UnconfirmedTransactionsWithoutReceivedCoinsShouldNotShowUp()
+		{
+			BlockchainBuilder builder = new BlockchainBuilder();
+			Tracker tracker = new Tracker();
+
+			Key bob = new Key();
+			tracker.Add(bob);
+			Key alice = new Key();
+
+			var tx1 = builder.GiveMoney(bob, Money.Coins(1.0m));
+			var b = builder.FindBlock();
+			tracker.NotifyTransaction(tx1, builder.Chain.Tip, b);
+
+			var tx2 = builder.SpendCoin(tx1.Outputs.AsCoins().First(), alice, Money.Coins(0.1m));
+			b = builder.FindBlock();
+			tracker.NotifyTransaction(tx2, builder.Chain.Tip, b);
+
+			var tx3 = builder.SpendCoin(tx2.Outputs.AsCoins().Skip(1).First(), alice, Money.Coins(0.2m));
+			Assert.True(tracker.NotifyTransaction(tx3));
+
+			var transactions = tracker.GetWalletTransactions(builder.Chain);
+			Assert.True(transactions.Count == 3);
+			Assert.True(transactions.Summary.UnConfirmed.TransactionCount == 1);
+			Assert.True(transactions.Summary.UnConfirmed.Amount == -Money.Coins(0.2m));
+
+			Assert.True(transactions.Summary.Confirmed.TransactionCount == 2);
+			Assert.True(transactions.Summary.Confirmed.Amount == Money.Coins(0.9m));
+
+			Assert.True(transactions.Summary.Spendable.TransactionCount == 3);
+			Assert.True(transactions.Summary.Spendable.Amount == Money.Coins(0.7m));
+
+			builder.Chain.SetTip(builder.Chain.GetBlock(1));
+
+			transactions = tracker.GetWalletTransactions(builder.Chain);
+
+			Action _ = () =>
+			{
+				Assert.True(transactions.Count == 3);
+				Assert.True(transactions.Summary.Confirmed.TransactionCount == 1);
+				Assert.True(transactions.Summary.Confirmed.Amount == Money.Coins(1.0m));
+				Assert.True(transactions.Summary.Spendable.TransactionCount == 3);
+				Assert.True(transactions.Summary.Spendable.Amount == Money.Coins(0.7m));
+			};
+			_();
+			tracker.NotifyTransaction(tx2);	//Notifying tx2 should have no effect, since it already is accounted because it was orphaned
+			_();
 		}
 	}
 }
