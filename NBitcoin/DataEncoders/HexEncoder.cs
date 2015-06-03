fc7bbdc64aa65ca9e212c27a3de5d59a24c8bad6
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-06-03 03:34:44 +0200

    optimize IsDigit

--- NBitcoin/DataEncoders/HexEncoder.cs
+++ NBitcoin/DataEncoders/HexEncoder.cs
@@ -16,15 +16,16 @@ namespace NBitcoin.DataEncoders
 
 		public override string EncodeData(byte[] data, int offset, int count)
 		{
-			if (data == null)
+			if(data == null)
 				throw new ArgumentNullException("data");
 
 			int pos = 0;
 			var spaces = (Space ? Math.Max((count - 1), 0) : 0);
 			var s = new char[2 * count + spaces];
-			for (var i = offset; i < offset + count; i++)
+			for(var i = offset ; i < offset + count ; i++)
 			{
-				if (Space && i != 0) s[pos++] = ' ';
+				if(Space && i != 0)
+					s[pos++] = ' ';
 				var c = HexTbl[data[i]];
 				s[pos++] = c[0];
 				s[pos++] = c[1];
@@ -34,30 +35,49 @@ namespace NBitcoin.DataEncoders
 
 		public override byte[] DecodeData(string encoded)
 		{
-			if (encoded == null)
+			if(encoded == null)
 				throw new ArgumentNullException("encoded");
-			if (encoded.Length % 2 == 1)
+			if(encoded.Length % 2 == 1)
 				throw new FormatException("Invalid Hex String");
 
 			var result = new byte[encoded.Length / 2];
-			for (int i = 0, j = 0; i < encoded.Length; i += 2, j++)
+			for(int i = 0, j = 0 ; i < encoded.Length ; i += 2, j++)
 			{
 				var a = IsDigit(encoded[i]);
 				var b = IsDigit(encoded[i + 1]);
-				if (a == -1 || b == -1)
+				if(a == -1 || b == -1)
 					throw new FormatException("Invalid Hex String");
 				result[j] = (byte)(((uint)a << 4) | (uint)b);
-			} return result;
+			}
+			return result;
 		}
 
-		static readonly char[] hexDigits = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F' };
-		static readonly byte[] hexValues = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 10, 11, 12, 13, 14, 15 };
+		static HexEncoder()
+		{
+			var hexDigits = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 
+																				  'A', 'B', 'C', 'D', 'E', 'F' };
+			var hexValues = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+																   10, 11, 12, 13, 14, 15};
+
+			var max = hexDigits.Max();
+			hexValueArray = new int[max + 1];
+			for(int i = 0 ; i < hexValueArray.Length ; i++)
+			{
+				var idx = Array.IndexOf(hexDigits, (char)i);
+				var value = -1;
+				if(idx != -1)
+					value = hexValues[idx];
+				hexValueArray[i] = value;
+			}
+		}
+
+		static readonly int[] hexValueArray;
+
 		public static int IsDigit(char c)
 		{
-			var i = Array.IndexOf(hexDigits, c);
-			if (i == -1)
+			if((int)c + 1 > hexValueArray.Length)
 				return -1;
-			return hexValues[i];
+			return hexValueArray[(int)c];
 		}
 
 		public static bool IsWellFormed(string str)
@@ -67,7 +87,7 @@ namespace NBitcoin.DataEncoders
 				Encoders.Hex.DecodeData(str);
 				return true;
 			}
-			catch (FormatException)
+			catch(FormatException)
 			{
 				return false;
 			}
