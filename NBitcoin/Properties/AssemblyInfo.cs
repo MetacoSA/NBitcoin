86f3727eaacda0babd32d634b32acb87276db8f8
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-06-04 11:16:40 +0200

    forget to update one assemblyinfo version

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.3.0")]
 [assembly: AssemblyFileVersion("2.0.3.0")]
-[assembly: AssemblyInformationalVersion("2.0.2.0")]
+[assembly: AssemblyInformationalVersion("2.0.3.0")]
