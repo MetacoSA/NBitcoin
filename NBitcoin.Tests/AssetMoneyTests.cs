f26fc7b95070e873a3d5274f6101c9c93c4b99ed
Lucas Cullen <support@dugongsoftware.com>
2015-07-01 06:57:33 +1000

    Asset money tests

--- NBitcoin.Tests/AssetMoneyTests.cs
+++ NBitcoin.Tests/AssetMoneyTests.cs
@@ -19,5 +19,44 @@ namespace NBitcoin.Tests
             String actual = assetMoney.ToString();
             Assert.Equal("1-8f316d9a09", actual);
         }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void AssetMoneyMultiply()
+        {
+            OpenAsset.AssetId assetId = new OpenAsset.AssetId("8f316d9a09");
+            OpenAsset.AssetMoney assetMoney = new OpenAsset.AssetMoney(assetId, 2);
+
+            OpenAsset.AssetMoney actual = assetMoney * 2;
+
+            Assert.Equal(4, actual.Quantity);
+
+            actual = 2 * assetMoney;
+            Assert.Equal(4, actual.Quantity);
+        }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void AssetMoneyGreaterThan()
+        {
+            OpenAsset.AssetId assetId = new OpenAsset.AssetId("8f316d9a09");
+            OpenAsset.AssetMoney smallAssetMoney = new OpenAsset.AssetMoney(assetId, 2);
+            OpenAsset.AssetMoney largeAssetMoney = new OpenAsset.AssetMoney(assetId, 5);
+
+            Assert.True(largeAssetMoney > smallAssetMoney);
+            Assert.False(smallAssetMoney > largeAssetMoney);
+        }
+
+        [Fact]
+        [Trait("UnitTest", "UnitTest")]
+        public void AssetMoneyLessThan()
+        {
+            OpenAsset.AssetId assetId = new OpenAsset.AssetId("8f316d9a09");
+            OpenAsset.AssetMoney smallAssetMoney = new OpenAsset.AssetMoney(assetId, 2);
+            OpenAsset.AssetMoney largeAssetMoney = new OpenAsset.AssetMoney(assetId, 5);
+
+            Assert.True(smallAssetMoney < largeAssetMoney);
+            Assert.False(largeAssetMoney < smallAssetMoney);
+        }
     }
 }
