4ee891145e73ce169ec3f25f63061e1b31527882
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-07-10 01:54:26 +0900

    fix Mono package

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -3,9 +3,10 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin\NBitcoin.
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(Profile111).csproj" -p:Configuration=Release
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(Profile259).csproj" -p:Configuration=Release
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin(MonoAndroid).csproj" -p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin(Mono).csproj" -p:Configuration=Release
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.csproj"
 
-nuGet pack NBitcoin.nuspec
+#nuGet pack NBitcoin.nuspec
 nuGet pack NBitcoin.Mono.nuspec
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
