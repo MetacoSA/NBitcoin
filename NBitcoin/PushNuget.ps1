3bb6899d11f136ccf7ed91b7c641d251f9e703c6
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-07-09 19:53:18 +0900

    fix push script

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -9,7 +9,7 @@ nuGet pack NBitcoin.nuspec
 nuGet pack NBitcoin.Mono.nuspec
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE"
-(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+(((dir *.nupkg).Name).Item(0) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
