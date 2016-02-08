5f67cc598825b13c5256a43fe513c0348342df04
Nicolas DORIER <nicolas.dorier@gmail.com>
2016-02-08 22:02:42 +0900

    Added BIP142 test

--- NBitcoin.Tests/transaction_tests.cs
+++ NBitcoin.Tests/transaction_tests.cs
@@ -1490,6 +1490,14 @@ namespace NBitcoin.Tests
 		}
 		[Fact]
 		[Trait("UnitTest", "UnitTest")]
+		public void bip142Test()
+		{
+			Transaction tx = new Transaction("0100000002fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f0000000000eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac11000000");
+			var h = Script.SignatureHash(new Script(Encoders.Hex.DecodeData("76a9141d0f172a0ecb48aee1be1f2687d2963ae33f71a188ac")), tx, 1, SigHash.All, Money.Satoshis(0x23c34600L), HashVersion.Witness);
+			Assert.Equal(new uint256(Encoders.Hex.DecodeData("c37af31116d1b27caf68aae9e3ac82f1477929014d5b917657d0eb49478cb670"), true), h);
+		}
+		[Fact]
+		[Trait("UnitTest", "UnitTest")]
 		public void witnessHasPushSizeLimit()
 		{
 			Key bob = new Key();
