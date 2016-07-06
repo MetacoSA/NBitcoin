f87536dc11cd4bd9070d28d8e09e4f537d543496
NicolasDorier <nicolas.dorier@gmail.com>
2016-07-07 07:54:44 +0900

    Fix Newtonsoft json dependency, version bump

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.0.35")]
-[assembly: AssemblyFileVersion("3.0.0.35")]
-[assembly: AssemblyInformationalVersion("3.0.0.35")]
+[assembly: AssemblyVersion("3.0.0.36")]
+[assembly: AssemblyFileVersion("3.0.0.36")]
+[assembly: AssemblyInformationalVersion("3.0.0.36")]
