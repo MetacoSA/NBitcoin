5def5b43df25ba52fd6587927f7fe10e90b27912
NicolasDorier <nicolas.dorier@gmail.com>
2016-08-10 14:22:56 +0900

    Profile259 should depends on Newtonsoft.Json, version bump

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.0.45")]
-[assembly: AssemblyFileVersion("3.0.0.45")]
-[assembly: AssemblyInformationalVersion("3.0.0.45")]
+[assembly: AssemblyVersion("3.0.0.46")]
+[assembly: AssemblyFileVersion("3.0.0.46")]
+[assembly: AssemblyInformationalVersion("3.0.0.46")]
