4b7f1c3779c8f92d935e24b224224a816bdb1ba0
NicolasDorier <nicolas.dorier@gmail.com>
2016-08-23 16:53:43 +0900

    minor fix build system

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -13,7 +13,7 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.cspro
 nuGet pack NBitcoin.nuspec
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
-(((dir *.nupkg).Name).Item(0) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
