811d75608d3b354dc16013a251054261b33cb646
NicolasDorier <nicolas.dorier@gmail.com>
2016-08-23 16:19:24 +0900

    Remove Portable lib support

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -1,9 +1,5 @@
 del *.nupkg
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin\NBitcoin.csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(Profile111).csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin.Portable(Profile259).csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin(MonoAndroid).csproj" -p:Configuration=Release
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin.Portable\NBitcoin(Mono).csproj" -p:Configuration=Release
 cd ..\NBitcoin.NETCore
 dotnet restore
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.csproj" /t:SetCoreVersion
@@ -12,10 +8,9 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.cspro
 cd ..\NBitcoin
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.csproj"
 
-.\GitLink.exe ".." -ignore "nbitcoin.portable.tests,common,nbitcoin.tests,newtonsoft.json.portable"
+.\GitLink.exe ".." -ignore "nbitcoin.portable.tests,common,nbitcoin.tests,build"
 
 nuGet pack NBitcoin.nuspec
-nuGet pack NBitcoin.Mono.nuspec
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
 (((dir *.nupkg).Name).Item(0) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
