7d39b541a7fc27fd5557d06ad78582db326bc027
NicolasDorier <nicolas.dorier@gmail.com>
2016-08-26 10:46:13 +0900

    WalletTransactionsCollection.GetSpendableCoins() throws if conflicting transaction, version bump

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.0.53")]
-[assembly: AssemblyFileVersion("3.0.0.53")]
-[assembly: AssemblyInformationalVersion("3.0.0.53")]
+[assembly: AssemblyVersion("3.0.0.54")]
+[assembly: AssemblyFileVersion("3.0.0.54")]
+[assembly: AssemblyInformationalVersion("3.0.0.54")]
