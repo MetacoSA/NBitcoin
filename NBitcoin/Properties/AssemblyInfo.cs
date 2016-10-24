1d74be5109fe45f671a1c5617f2b9ae0df28cd39
NicolasDorier <nicolas.dorier@gmail.com>
2016-10-25 00:14:59 +0900

    bump version

--- NBitcoin/Properties/AssemblyInfo.cs
+++ NBitcoin/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.0.62")]
-[assembly: AssemblyFileVersion("3.0.0.62")]
-[assembly: AssemblyInformationalVersion("3.0.0.62")]
+[assembly: AssemblyVersion("3.0.0.63")]
+[assembly: AssemblyFileVersion("3.0.0.63")]
+[assembly: AssemblyInformationalVersion("3.0.0.63")]
