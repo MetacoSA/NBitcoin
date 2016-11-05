551365dc500605a0cec7a02755c15a679f1c2ca2
Dan Gershony <dan.gershony@gmail.com>
2016-11-05 21:44:48 +0000

    Exclude the custom implementations

--- Hashing/HashLibTest/HashesTest.cs
+++ Hashing/HashLibTest/HashesTest.cs
@@ -582,18 +582,21 @@ namespace HashLibTest
         [TestMethod]
         public void TestHashes()
         {
+			// exclude the custom implementations
+			var excludeCustom = new Func<Type, bool>(type => !type?.Namespace?.Contains(".Custom") ?? false);
+	        
             Assert.AreEqual(
                 "F3581AE5-027E0B67-5FB9136F-E8C1403E-4DF209FA-2C07CAC5-D8A7BEC8-DC9076C0-63B2DEEC-9DA04A78-DC3DE302-A99ACC2C-B79333E3-FE4C7F16-50691C22-1180354B", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.All.Select(el => el.GetType().FullName).
+                        Hashes.All.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "A27A6429-71247FF0-CC8BC923-DEE3E261-6EBAA40A-7872BC00-3B280C7D-5C7D3D2C-D5539A29-9876F73B-AE10CAA4-11AB9A21-B0C7F915-C877B390-C3A6695D-21428416", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.AllUnique.Select(el => el.GetType().FullName).
+                        Hashes.AllUnique.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
@@ -607,70 +610,70 @@ namespace HashLibTest
                 "6C1E48E2-74A976FD-7A9F76E1-2F784912-929B939C-19E7B195-65CE29FD-9181D337-2185BB77-F86B16B3-FB6638C2-3A8D130B-BEC517A7-FC639305-28A079D1-D5D115DE", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.CryptoAll.Select(el => el.GetType().FullName).
+                        Hashes.CryptoAll.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "87D9E63B-B68DC8B9-2B970BD8-503D861D-4C406A3A-9BF28A7F-189C7E85-D72F1528-3279F0D7-79CF9B36-396B5F98-DDE3FE0A-A47243FE-912A1E8B-E308CA0A-5A8844F0", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.CryptoBuildIn.Select(el => el.GetType().FullName).
+                        Hashes.CryptoBuildIn.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "37B94613-2D4E7794-6E763DAE-AD5CD38E-5AA88F82-DD96A845-D71DFDC7-A28DC559-DEBD055A-1A154837-9C66C6F7-9BF1B0E6-6EA3263B-E1FAF1BA-C0624119-AC4382DA", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.CryptoNotBuildIn.Select(el => el.GetType().FullName).
+                        Hashes.CryptoNotBuildIn.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "8F2F5764-A203761B-8B84755B-206C3255-1DD29088-90F5AE37-7879460D-00B2EF42-195C8FB1-9C0DEDD7-153A6E7B-5F447E15-BF6D4B53-7B95F73B-80BE1C34-2972CCF4", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.FastComputes.Select(el => el.GetType().FullName).
+                        Hashes.FastComputes.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "48DDCBA3-AF1BB119-B1358345-3933130B-D19D756C-A4280134-C3A43180-401F6D72-DB52234D-3CF5232D-1202A332-89B46C77-B8239B28-A4A6A279-D2082CF5-CFE67514", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.Hash32.Select(el => el.GetType().FullName).
+                        Hashes.Hash32.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "13F849F0-C5D6743F-B6342FAE-FD101384-72705F8B-412E9C2E-F3B7BD4B-56BAF8EC-F2397B9E-8CDC52FD-5F999591-214E2499-15F24A43-9147FFAB-EDAB01CD-0CA2126D", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.Hash64.Select(el => el.GetType().FullName).
+                        Hashes.Hash64.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "7133CF20-EF5E24ED-7B97CA25-AE4E6867-0F9C22AC-B7ECB12C-4D0A482D-0D06D08A-69567E42-24B3440A-224DA240-CC282BF0-A4793F3B-75DBEC35-E7CCF361-3C70F05B", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.Hash128.Select(el => el.GetType().FullName).
+                        Hashes.Hash128.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "67F03D8C-380AD001-25719A65-14D96D5F-CBA29A9A-E282977B-C6B39696-70F3E0CF-63FDD4F1-07B35AB7-CB9552D0-28981B10-FD6CDB78-91EB3C33-91D10D03-2EF6C2E2", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.HasHMACBuildIn.Select(el => el.GetType().FullName).
+                        Hashes.HasHMACBuildIn.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "A44F5451-85495868-48531E32-8953390D-BEA98D72-CE1A3D9F-72D56E2D-C90D79CE-53E9E384-9636CC43-B9498F19-583F1AF8-9E6F5CA7-10D28145-327A87CC-C3E75113", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.NonBlock.Select(el => el.GetType().FullName).
+                        Hashes.NonBlock.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
 
             Assert.AreEqual(
                 "E578F7C4-5CEBA02B-7CC51716-23E46666-CC43875C-4C6288DC-48B1DB75-9A11C6CD-C2B88CE4-96F052A0-A0D8BFDF-F31D6B38-AE57BEB3-9A0310FA-A9ECCC26-DB17FD4A", 
                 HashFactory.Crypto.BuildIn.CreateSHA512Cng().ComputeBytes(
                     Converters.ConvertStringToBytes(
-                        Hashes.WithKey.Select(el => el.GetType().FullName).
+                        Hashes.WithKey.Where(excludeCustom).Select(el => el.GetType().FullName).
                             Aggregate("", (acc, val) => acc += val))).ToString());
         }
 
