4f2799e4b2e77c704866e8dd823138fa508129c8
Sly Gryphon <sgryphon@computer.org>
2016-11-17 12:34:13 +1000

    Convert temporary clone into partial class, deleting the unneeded parts of each file. Note that the GetTransactions() method is actually part of blockchain and not the gettransaction function from wallet. Also getaccount/getaccountaddress were labelled wrong.

--- NBitcoin/RPC/RPCClient.cs
+++ NBitcoin/RPC/RPCClient.cs
@@ -71,7 +71,7 @@ namespace NBitcoin.RPC
 		}
 	}
 
-	/*
+    /*
         Category            Name                        Implemented 
         ------------------ --------------------------- -----------------------
         ------------------ Overall control/query calls 
@@ -148,15 +148,15 @@ namespace NBitcoin.RPC
         wallet             dumpprivkey                  Yes
         wallet             dumpwallet
         wallet             encryptwallet
-        wallet             getaccountaddress
-        wallet             getaccount                   Yes
+        wallet             getaccountaddress            Yes
+        wallet             getaccount
         wallet             getaddressesbyaccount
         wallet             getbalance
         wallet             getnewaddress
         wallet             getrawchangeaddress
         wallet             getreceivedbyaccount
         wallet             getreceivedbyaddress
-        wallet             gettransaction               Yes
+        wallet             gettransaction
         wallet             getunconfirmedbalance
         wallet             getwalletinfo
         wallet             importprivkey                Yes
@@ -183,7 +183,7 @@ namespace NBitcoin.RPC
         wallet             walletpassphrasechange
         wallet             walletpassphrase
     */
-	public class RPCClient : IBlockRepository
+    public partial class RPCClient : IBlockRepository
 	{
 		private readonly NetworkCredential _credentials;
 		public NetworkCredential Credentials
@@ -633,15 +633,44 @@ namespace NBitcoin.RPC
 			return array.Select(o => (string)o).Select(uint256.Parse).ToArray();
 		}
 
-		#endregion
+        /// <summary>
+        /// GetTransactions only returns on txn which are not entirely spent unless you run bitcoinq with txindex=1.
+        /// </summary>
+        /// <param name="blockHash"></param>
+        /// <returns></returns>
+        public IEnumerable<Transaction> GetTransactions(uint256 blockHash)
+        {
+            if (blockHash == null)
+                throw new ArgumentNullException("blockHash");
 
-		#region Coin generation
+            var resp = SendCommand("getblock", blockHash.ToString());
 
-		#endregion
+            var tx = resp.Result["tx"] as JArray;
+            if (tx != null)
+            {
+                foreach (var item in tx)
+                {
+                    var result = GetRawTransaction(uint256.Parse(item.ToString()), false);
+                    if (result != null)
+                        yield return result;
+                }
+            }
+        }
 
-		#region Raw Transaction
+        public IEnumerable<Transaction> GetTransactions(int height)
+        {
+            return GetTransactions(GetBlockHash(height));
+        }
 
-		public Transaction DecodeRawTransaction(string rawHex)
+        #endregion
+
+        #region Coin generation
+
+        #endregion
+
+        #region Raw Transaction
+
+        public Transaction DecodeRawTransaction(string rawHex)
 		{
 			var response = SendCommand("decoderawtransaction", rawHex);
 			return Transaction.Parse(response.Result.ToString(), RawFormat.Satoshi);
@@ -713,278 +742,6 @@ namespace NBitcoin.RPC
 		public Task SendRawTransactionAsync(byte[] bytes)
 		{
 			return SendCommandAsync("sendrawtransaction", Encoders.Hex.EncodeData(bytes));
-		}
-
-		#endregion
-
-		#region Wallet
-
-		public void ImportPrivKey(BitcoinSecret secret)
-		{
-			SendCommand("importprivkey", secret.ToWif());
-		}
-
-		public void ImportPrivKey(BitcoinSecret secret, string label, bool rescan)
-		{
-			SendCommand("importprivkey", secret.ToWif(), label, rescan);
-		}
-
-		public async Task ImportPrivKeyAsync(BitcoinSecret secret)
-		{
-			await SendCommandAsync("importprivkey", secret.ToWif()).ConfigureAwait(false);
-		}
-
-		public async Task ImportPrivKeyAsync(BitcoinSecret secret, string label, bool rescan)
-		{
-			await SendCommandAsync("importprivkey", secret.ToWif(), label, rescan).ConfigureAwait(false);
-		}
-
-		public void ImportAddress(BitcoinAddress address)
-		{
-			SendCommand("importaddress", address.ToString());
-		}
-
-		public void ImportAddress(BitcoinAddress address, string label, bool rescan)
-		{
-			SendCommand("importaddress", address.ToString(), label, rescan);
-		}
-
-		public async Task ImportAddressAsync(BitcoinAddress address)
-		{
-			await SendCommandAsync("importaddress", address.ToString()).ConfigureAwait(false);
-		}
-
-		public async Task ImportAddressAsync(BitcoinAddress address, string label, bool rescan)
-		{
-			await SendCommandAsync("importaddress", address.ToString(), label, rescan).ConfigureAwait(false);
-		}
-
-		public BitcoinSecret DumpPrivKey(BitcoinAddress address)
-		{
-			var response = SendCommand("dumpprivkey", address.ToString());
-			return Network.CreateFromBase58Data<BitcoinSecret>((string)response.Result);
-		}
-
-		public async Task<BitcoinSecret> DumpPrivKeyAsync(BitcoinAddress address)
-		{
-			var response = await SendCommandAsync("dumpprivkey", address.ToString()).ConfigureAwait(false);
-			return Network.CreateFromBase58Data<BitcoinSecret>((string)response.Result);
-		}
-
-		public BitcoinAddress GetAccountAddress(string account)
-		{
-			var response = SendCommand("getaccountaddress", account);
-			return Network.CreateFromBase58Data<BitcoinAddress>((string)response.Result);
-		}
-
-		public async Task<BitcoinAddress> GetAccountAddressAsync(string account)
-		{
-			var response = await SendCommandAsync("getaccountaddress", account).ConfigureAwait(false);
-			return Network.CreateFromBase58Data<BitcoinAddress>((string)response.Result);
-		}
-
-		public BitcoinSecret GetAccountSecret(string account)
-		{
-			var address = GetAccountAddress(account);
-			return DumpPrivKey(address);
-		}
-
-		public async Task<BitcoinSecret> GetAccountSecretAsync(string account)
-		{
-			var address = await GetAccountAddressAsync(account).ConfigureAwait(false);
-			return await DumpPrivKeyAsync(address).ConfigureAwait(false);
-		}
-
-		public UnspentCoin[] ListUnspent()
-		{
-			var response = SendCommand("listunspent");
-			return response.Result.Select(i => new UnspentCoin((JObject)i)).ToArray();
-		}
-
-		public UnspentCoin[] ListUnspent(int minconf, int maxconf, params BitcoinAddress[] addresses)
-		{
-			var addr = from a in addresses select a.ToString();
-			var response = SendCommand("listunspent", minconf, maxconf, addr.ToArray());
-			return response.Result.Select(i => new UnspentCoin((JObject)i)).ToArray();
-		}
-
-		public async Task<UnspentCoin[]> ListUnspentAsync()
-		{
-			var response = await SendCommandAsync("listunspent").ConfigureAwait(false);
-			return response.Result.Select(i => new UnspentCoin((JObject)i)).ToArray();
-		}
-
-		public async Task<UnspentCoin[]> ListUnspentAsync(int minconf, int maxconf, params BitcoinAddress[] addresses)
-		{
-			var addr = from a in addresses select a.ToString();
-			var response = await SendCommandAsync("listunspent", minconf, maxconf, addr.ToArray()).ConfigureAwait(false);
-			return response.Result.Select(i => new UnspentCoin((JObject)i)).ToArray();
-		}
-
-		public void LockUnspent(params OutPoint[] outpoints)
-		{
-			LockUnspentCore(false, outpoints);
-		}
-
-		public void UnlockUnspent(params OutPoint[] outpoints)
-		{
-			LockUnspentCore(true, outpoints);
-		}
-
-		public Task LockUnspentAsync(params OutPoint[] outpoints)
-		{
-			return LockUnspentCoreAsync(false, outpoints);
-		}
-
-		public Task UnlockUnspentAsync(params OutPoint[] outpoints)
-		{
-			return LockUnspentCoreAsync(true, outpoints);
-		}
-
-		private void LockUnspentCore(bool unlock, OutPoint[] outpoints)
-		{
-			try
-			{
-				LockUnspentCoreAsync(unlock, outpoints).Wait();
-			}
-			catch(AggregateException ex)
-			{
-				ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
-			}
-		}
-
-		private async Task LockUnspentCoreAsync(bool unlock, OutPoint[] outpoints)
-		{
-			if(outpoints == null || outpoints.Length == 0)
-				return;
-			var parameters = new List<object>();
-			parameters.Add(unlock);
-			var array = new JArray();
-			parameters.Add(array);
-			foreach(var outp in outpoints)
-			{
-				var obj = new JObject();
-				obj["txid"] = outp.Hash.ToString();
-				obj["vout"] = outp.N;
-				array.Add(obj);
-			}
-			await SendCommandAsync("lockunspent", parameters.ToArray()).ConfigureAwait(false);
-		}
-
-		/// <summary>
-		/// GetTransactions only returns on txn which are not entirely spent unless you run bitcoinq with txindex=1.
-		/// </summary>
-		/// <param name="blockHash"></param>
-		/// <returns></returns>
-		public IEnumerable<Transaction> GetTransactions(uint256 blockHash)
-		{
-			if(blockHash == null)
-				throw new ArgumentNullException("blockHash");
-
-			var resp = SendCommand("getblock", blockHash.ToString());
-
-			var tx = resp.Result["tx"] as JArray;
-			if(tx != null)
-			{
-				foreach(var item in tx)
-				{
-					var result = GetRawTransaction(uint256.Parse(item.ToString()), false);
-					if(result != null)
-						yield return result;
-				}
-			}
-		}
-
-		public IEnumerable<Transaction> GetTransactions(int height)
-		{
-			return GetTransactions(GetBlockHash(height));
-		}
-
-        /// <summary>
-        /// Lists accounts and their balances
-        /// </summary>
-        public IEnumerable<RPCAccount> ListAccounts()
-        {
-            return ListAccounts(1, false);
-        }
-
-        /// <summary>
-        /// Lists accounts and their balances
-        /// </summary>
-        /// <param name="confirmations">
-        /// The minimum number of confirmations an externally-generated transaction must have before 
-        /// it is counted towards the balance. Transactions generated by this node are counted immediately. 
-        /// Typically, externally-generated transactions are payments to this wallet and transactions 
-        /// generated by this node are payments to other wallets. Use 0 to count unconfirmed transactions. 
-        /// Default is 1
-        /// </param>
-        /// <param name="includeWatchOnly">
-        /// If set to true, include watch-only addresses in details and calculations as if they were 
-        /// regular addresses belonging to the wallet. If set to false (the default), treat watch-only 
-        /// addresses as if they didn’t belong to this wallet.
-        /// </param>
-        /// <returns>
-        /// A list of accounts and their balances
-        /// </returns>
-        public IEnumerable<RPCAccount> ListAccounts(int confirmations = 1, bool includeWatchOnly = false)
-		{
-			var result = SendCommand(RPCOperations.listaccounts, confirmations, includeWatchOnly);
-			var obj = (JObject)result.Result;
-			foreach(var prop in obj.Properties())
-			{
-				yield return new RPCAccount()
-				{
-					AccountName = prop.Name,
-					Amount = Money.Coins((decimal)prop.Value)
-				};
-			}
-		}
-
-		public void BackupWallet(string path)
-		{
-			if(string.IsNullOrEmpty(path))
-				throw new ArgumentNullException("path");
-			SendCommand("backupwallet", path);
-		}
-
-		public async Task BackupWalletAsync(string path)
-		{
-			if(string.IsNullOrEmpty(path))
-				throw new ArgumentNullException("path");
-			await SendCommandAsync("backupwallet", path).ConfigureAwait(false);
-		}
-
-		public IEnumerable<BitcoinSecret> ListSecrets()
-		{
-			foreach(var grouping in ListAddressGroupings())
-			{
-				yield return DumpPrivKey(grouping.PublicAddress);
-				foreach(var change in grouping.ChangeAddresses)
-					yield return DumpPrivKey(change.Address);
-			}
-		}
-
-		public IEnumerable<AddressGrouping> ListAddressGroupings()
-		{
-			var result = SendCommand(RPCOperations.listaddressgroupings);
-			var array = (JArray)result.Result;
-			foreach(var group in array.Children<JArray>())
-			{
-				var grouping = new AddressGrouping();
-				grouping.PublicAddress = BitcoinAddress.Create(group[0][0].ToString());
-				grouping.Amount = Money.Coins(group[0][1].Value<decimal>());
-				grouping.Account = group[0].Count() > 2 ? group[0][2].ToString() : null;
-
-				foreach(var subgroup in group.Skip(1))
-				{
-					var change = new ChangeAddress();
-					change.Address = BitcoinAddress.Create(subgroup[0].ToString());
-					change.Amount = Money.Coins(subgroup[1].Value<decimal>());
-					grouping.ChangeAddresses.Add(change);
-				}
-
-				yield return grouping;
-			}
 		}
 
 		#endregion
