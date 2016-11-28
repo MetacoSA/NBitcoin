6cb177ea94354ae65c659fe8db5397bf67eaed8a
Dan Gershony <dan.gershony@gmail.com>
2016-11-28 22:41:41 +0000

    Deploy to nuget

--- nStratis/PushNuget.ps1
+++ nStratis/PushNuget.ps1
@@ -1,16 +1,16 @@
 del *.nupkg
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\NBitcoin\NBitcoin.csproj" -p:Configuration=Release
-cd ..\NBitcoin.NETCore
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\nStratis\nStratis.csproj" -p:Configuration=Release
+cd ..\nStratis.NETCore
 dotnet restore
 dotnet build -c Release
-cd ..\NBitcoin
+cd ..\nStratis
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.csproj"
 
-.\GitLink.exe ".." -ignore "nbitcoin.portable.tests,common,nbitcoin.tests,build"
+.\GitLink.exe ".." -ignore "nStratis.portable.tests,common,nStratis.tests,build"
 
-nuGet pack NBitcoin.nuspec
+..\.nuget\nuGet pack nStratis.nuspec
 
-forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
+forfiles /m *.nupkg /c "cmd /c ..\.nuget\NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
 (((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
