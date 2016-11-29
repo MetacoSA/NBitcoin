2e4ac1e0b4af444156e75d09bbf0b4a9c90ccc23
Dan Gershony <dan.gershony@gmail.com>
2016-11-29 13:21:20 +0000

    Upgrade MSBuild version and documentation Deployment is now fully automated

--- nStratis/PushNuget.ps1
+++ nStratis/PushNuget.ps1
@@ -1,17 +1,28 @@
 del *.nupkg
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\nStratis\nStratis.csproj" -p:Configuration=Release
+
+## build the project
+C:\"Program Files (x86)"\MSBuild\14.0\Bin\msbuild.exe  "..\nStratis\nStratis.csproj" -p:Configuration=Release
+
+## build in netcore
 cd ..\nStratis.NETCore
 dotnet restore
 dotnet build -c Release
+
+## create the nuspec file
 cd ..\nStratis
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "..\Build\Deploy.csproj"
 
+## enable debug in to the package by including the .pdb files
 .\GitLink.exe ".." -ignore "nStratis.portable.tests,common,nStratis.tests,build"
 
+## package the code
 ..\.nuget\nuGet pack nStratis.nuspec
 
+## push to nuget
 forfiles /m *.nupkg /c "cmd /c ..\.nuget\NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
-(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
-$ver = $Matches.Item(0)
-git tag -a "v$ver" -m "$ver"
-git push --tags
+
+## publish a tag to git
+#(((dir *.nupkg).Name) -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
+#$ver = $Matches.Item(0)
+#git tag -a "v$ver" -m "$ver"
+#git push --tags
