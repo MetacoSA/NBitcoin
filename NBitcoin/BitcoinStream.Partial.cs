b132d46e9faa40d917de5eea669e22b57a641b5f
Dan Gershony <dan.gershony@gmail.com>
2017-03-22 22:18:15 +0000

    folder change

--- NBitcoin/BitcoinStream.Partial.cs
+++ NBitcoin/BitcoinStream.Partial.cs
@@ -1,5 +1,5 @@
 ﻿
-using NBitcoin.Protocol;
+using nStratis.Protocol;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,7 +9,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NBitcoin
+namespace nStratis
 {
 	public partial class BitcoinStream
 	{
