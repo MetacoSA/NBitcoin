ef3817c1ba55f247e0a1ca95b8391a2c5decb6f7
NicolasDorier <nicolas.dorier@gmail.com>
2018-02-21 12:32:52 +0900

    Change minRelayFee back to 1000 satoshi per kb

--- NBitcoin/Policy/StandardTransactionPolicy.cs
+++ NBitcoin/Policy/StandardTransactionPolicy.cs
@@ -13,7 +13,7 @@ namespace NBitcoin.Policy
 			ScriptVerify = NBitcoin.ScriptVerify.Standard;
 			MaxTransactionSize = 100000;
 			MaxTxFee = new FeeRate(Money.Coins(0.1m));
-			MinRelayTxFee = new FeeRate(Money.Satoshis(5000));
+			MinRelayTxFee = new FeeRate(Money.Satoshis(1000), 1000);
 			CheckFee = true;
 			CheckScriptPubKey = true;
 		}
