60ee60d88d1e17a6c92961bf8ee37495e2b3151e
NicolasDorier <nicolas.dorier@gmail.com>
2018-04-06 19:29:14 +0900

    Fix tag format

--- NBitcoin.TestFramework/PushNuget.ps1
+++ NBitcoin.TestFramework/PushNuget.ps1
@@ -3,5 +3,5 @@ dotnet pack --configuration Release
 
 dotnet nuget push "bin\Release\*.nupkg" --source "https://api.nuget.org/v3/index.json"
 $ver = ((ls .\bin\release -File)[0].Name -replace '([^\.\d]*\.)+(\d+(\.\d+){1,3}).*', '$2')
-git tag -a "NBitcoin.TestFramework\v$ver" -m "NBitcoin.TestFramework/$ver"
+git tag -a "NBitcoin.TestFramework/v$ver" -m "NBitcoin.TestFramework/$ver"
 git push --tags
