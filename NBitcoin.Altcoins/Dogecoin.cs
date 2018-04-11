414899cbf94d121e98b7b704b3c650627042bc45
Bub <bdangh@danghyan.info>
2018-04-11 20:10:50 -0300

    Fixed Dogecoin AuxPow header
    Fixed Dogecoin chain synchronization issue because of hard-fork at 371337rd block.
    
    https://github.com/dogecoin/dogecoin/blob/bb4b082c086689434d67490a15224c42fefdfd13/src/auxpow.h

--- NBitcoin.Altcoins/Dogecoin.cs
+++ NBitcoin.Altcoins/Dogecoin.cs
@@ -27,13 +27,153 @@ namespace NBitcoin.Altcoins
 		}
 
 #pragma warning disable CS0618 // Type or member is obsolete
+		public class AuxPow : IBitcoinSerializable
+		{
+			Transaction tx = new Transaction();
+
+			public Transaction Transactions
+			{
+				get
+				{
+					return tx;
+				}
+				set
+				{
+					tx = value;
+				}
+			}
+
+			uint nIndex = 0;
+
+			public uint Index
+			{
+				get
+				{
+					return nIndex;
+				}
+				set
+				{
+					nIndex = value;
+				}
+			}
+
+			uint256 hashBlock = new uint256();
+
+			public uint256 HashBlock
+			{
+				get
+				{
+					return hashBlock;
+				}
+				set
+				{
+					hashBlock = value;
+				}
+			}
+
+			List<uint256> vMerkelBranch = new List<uint256>();
+
+			public List<uint256> MerkelBranch
+			{
+				get
+				{
+					return vMerkelBranch;
+				}
+				set
+				{
+					vMerkelBranch = value;
+				}
+			}
+
+			List<uint256> vChainMerkleBranch = new List<uint256>();
+
+			public List<uint256> ChainMerkleBranch
+			{
+				get
+				{
+					return vChainMerkleBranch;
+				}
+				set
+				{
+					vChainMerkleBranch = value;
+				}
+			}
+
+			uint nChainIndex = 0;
+
+			public uint ChainIndex
+			{
+				get
+				{
+					return nChainIndex;
+				}
+				set
+				{
+					nChainIndex = value;
+				}
+			}
+
+			BlockHeader parentBlock = new BlockHeader();
+
+			public BlockHeader ParentBlock
+			{
+				get
+				{
+					return parentBlock;
+				}
+				set
+				{
+					parentBlock = value;
+				}
+			}
+
+			public void ReadWrite(BitcoinStream stream)
+			{
+				stream.ReadWrite(ref tx);
+				stream.ReadWrite(ref hashBlock);
+				stream.ReadWrite(ref vMerkelBranch);
+				stream.ReadWrite(ref nIndex);
+				stream.ReadWrite(ref vChainMerkleBranch);
+				stream.ReadWrite(ref nChainIndex);
+				stream.ReadWrite(ref parentBlock);
+			}
+		}
+
 		public class DogecoinBlockHeader : BlockHeader
 		{
+			const int VERSION_AUXPOW = (1 << 8);
+
+			AuxPow auxPow = new AuxPow();
+
+			public AuxPow AuxPow
+			{
+				get
+				{
+					return auxPow;
+				}
+				set
+				{
+					auxPow = value;
+				}
+			}
+
 			public override uint256 GetPoWHash()
 			{
 				var headerBytes = this.ToBytes();
 				var h = NBitcoin.Crypto.SCrypt.ComputeDerivedKey(headerBytes, headerBytes, 1024, 1, 1, null, 32);
 				return new uint256(h);
+			}
+
+			public override void ReadWrite(BitcoinStream stream)
+			{
+				base.ReadWrite(stream);
+				if ((Version & VERSION_AUXPOW) != 0)
+				{
+					if (!stream.Serializing)
+					{
+						stream.ReadWrite(ref auxPow);
+					}
+				}
 			}
 		}
 #pragma warning restore CS0618 // Type or member is obsolete
