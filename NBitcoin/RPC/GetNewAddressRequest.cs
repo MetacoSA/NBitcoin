c2df8927058388791548c1675a09344aea8f7f0d
NicolasDorier <nicolas.dorier@gmail.com>
2018-06-22 23:11:33 +0900

    Support named parameter for RPCClient, support GetNewAddress with address type

---
+++ NBitcoin/RPC/GetNewAddressRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBitcoin.RPC
+{
+	public enum AddressType
+	{
+		Legacy,
+		P2SHSegwit,
+		Bech32
+	}
+    public class GetNewAddressRequest
+    {
+		public string Label
+		{
+			get; set;
+		}
+
+		public AddressType? AddressType
+		{
+			get; set;
+		}
+	}
+}
