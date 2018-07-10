9b69b2a8ed9d53529451ba887a7afe11667e8f7f
Lucas Ontivero <lucasontivero@gmail.com>
2018-07-10 16:30:42 +0100

    Small refactoring

--- NBitcoin/Crypto/SchnorrSignature.cs
+++ NBitcoin/Crypto/SchnorrSignature.cs
@@ -8,79 +8,98 @@ using System.Text;
 using System.Threading.Tasks;
 using NBitcoin.BouncyCastle.Asn1.X9;
 using System.Security.Cryptography;
+using NBitcoin.BouncyCastle.Math.EC.Custom.Sec;
+using NBitcoin.DataEncoders;
 
 namespace NBitcoin.Crypto
 {
+
+	class Utilsx
+	{
+		public static byte[] ToBytes(BigInteger me)
+		{
+			var buff = me.ToByteArrayUnsigned();
+			var zeros = 32 - buff.Length;
+			if(zeros==0)
+				return buff;
+			else if(zeros > 0)
+				return ByteArrayExtensions.Concat(new byte[zeros], buff);
+			else
+				return buff.SafeSubarray(0, 32);
+		}
+	}
+
+	public class SchnorrSignature
+	{
+		public BigInteger R { get; }
+		public BigInteger S { get; }
+
+		public static SchnorrSignature Parse(string hex)
+		{
+			var bytes = Encoders.Hex.DecodeData(hex);
+			return new SchnorrSignature(bytes);
+		}
+
+		public SchnorrSignature(byte[] bytes)
+		{
+			if(bytes.Length != 64)
+				throw new ArgumentException("Invalid schnorr signature lenght.");
+
+			R = new BigInteger(bytes.SafeSubarray(0,32));
+			S = new BigInteger(bytes.SafeSubarray(32,32));
+		}
+
+		public SchnorrSignature(BigInteger r, BigInteger s)
+		{
+			R = r;
+			S = s;
+		}
+
+		public byte[] ToBytes()
+		{
+			return Utilsx.ToBytes(R).Concat( Utilsx.ToBytes(S) );
+		}
+	}
+
 	public class SchnorrSigner
 	{
+		private static X9ECParameters Secp256k1 =  NBitcoin.BouncyCastle.Crypto.EC.CustomNamedCurves.Secp256k1;
+		private static BigInteger PP = ((SecP256K1Curve)Secp256k1.Curve).QQ;
 
-        public byte[] Sign(uint256 m, byte[] secret)
-        {
-            var qq = ((NBitcoin.BouncyCastle.Math.EC.Custom.Sec.SecP256K1Curve)ECKey.Secp256k1.Curve).QQ;
-            var key = new uint256(secret);
-            var keyPrefixedMessage =  key.ToBytes(true).Concat(m.ToBytes(false)); 
-            var k = new BigInteger(1, Hashes.SHA256(keyPrefixedMessage));
-            var R = ECKey.Secp256k1.G.Multiply(k);
-            var Xr = R.X.ToBigInteger();
-            var Yr = R.Y.ToBigInteger();
-            if( BigInteger.Jacobi(Yr, qq) != 1)
-                k = ECKey.Secp256k1.N.Subtract(k);
+		public SchnorrSignature Sign(uint256 m, BigInteger secret)
+		{
+			var k = new BigInteger(1, Hashes.SHA256(Utilsx.ToBytes(secret).Concat(m.ToBytes(false))));
+			var R = Secp256k1.G.Multiply(k).Normalize();
+			var (Xr, Yr) = (R.XCoord.ToBigInteger(), R.YCoord.ToBigInteger());
 
-            var P = ECKey.Secp256k1.G.Multiply(new BigInteger(1, secret)); 
-            var r = new uint256 (Xr.SignValue > 0 ? Xr.ToByteArray() : Xr.ToByteArrayUnsigned()).ToBytes(true);
-            var encP = P.GetEncoded(true);
-            var keyPrefixedM = ByteArrayExtensions.Concat(r, encP, m.ToBytes(false));
-            var e = new BigInteger(1, Hashes.SHA256(keyPrefixedM));
-            var ss= new BigInteger(1, secret);
+			if( BigInteger.Jacobi(Yr, PP) != 1)
+				k = Secp256k1.N.Subtract(k);
 
-            var t = k.Add(e.Multiply(ss)).Mod(ECKey.Secp256k1.N);
-            var sig = ByteArrayExtensions.Concat(r,  new uint256(t.SignValue>0 ? t.ToByteArray() : t.ToByteArrayUnsigned()).ToBytes(true));
-            return sig;
-        } 
+			var P = Secp256k1.G.Multiply(secret); 
+			var keyPrefixedM = Utilsx.ToBytes(Xr).Concat( P.GetEncoded(true), m.ToBytes(false) );
+			var e = new BigInteger(1, Hashes.SHA256(keyPrefixedM));
 
-        /*
-        def schnorr_verify(msg, pubkey, sig):
-            if (not on_curve(pubkey)):
-                return False
-            r = int.from_bytes(sig[0:32], byteorder="big")
-            s = int.from_bytes(sig[32:64], byteorder="big")
-            if r >= p or s >= n:
-                return False
-            e = sha256(sig[0:32] + bytes_point(pubkey) + msg)
-            R = point_add(point_mul(G, s), point_mul(pubkey, n - e))
-            if R is None or jacobi(R[1]) != 1 or R[0] != r:
-                return False
-            return True
-        */
+			var s = k.Add(e.Multiply(secret)).Mod(Secp256k1.N);
+			return new SchnorrSignature(Xr, s);
+		} 
 
-        public bool Verify(uint256 m, byte[] pubkey, byte[] sig)
-        {
-            var r = sig.SafeSubarray(0, 32);
-            var s = sig.SafeSubarray(32, 32);
-            var ri = new BigInteger(1, r);
-            var si = new BigInteger(1, s);
+		public bool Verify(uint256 m, byte[] pubkey, SchnorrSignature sig)
+		{
+			if( sig.R.CompareTo(PP)>=0 || sig.S.CompareTo(Secp256k1.N)>=0)
+				return false;
 
-            var secP256K1_P = ((NBitcoin.BouncyCastle.Math.EC.Custom.Sec.SecP256K1Curve)ECKey.Secp256k1.Curve).QQ;
+			var e = new BigInteger(1, Hashes.SHA256( Utilsx.ToBytes( sig.R ).Concat( pubkey, m.ToBytes(false))));
 
-            if( ri.CompareTo(secP256K1_P)>=0 || si.CompareTo(ECKey.Secp256k1.N)>=0)
-            {
-                return false;
-            }
-            var e = new BigInteger(1, Hashes.SHA256( ByteArrayExtensions.Concat(r, pubkey, m.ToBytes(false))));
+			var eckey = new ECKey(pubkey, false);
+			var q = eckey.GetPublicKeyParameters().Q.Normalize();
+			var P = Secp256k1.Curve.CreatePoint(q.XCoord.ToBigInteger(), q.YCoord.ToBigInteger());
 
-            var eckey = new ECKey(pubkey, false);
-			var q = eckey.GetPublicKeyParameters().Q;
-			q = q.Normalize();
-			var P = ECKey.Secp256k1.Curve.CreatePoint(q.XCoord.ToBigInteger(), q.YCoord.ToBigInteger());
+			var R = Secp256k1.G.Multiply(sig.S).Add(P.Multiply(Secp256k1.N.Subtract(e))).Normalize();
 
-            var Rt1 = ECKey.Secp256k1.G.Multiply(si);
-            var Rt2 = P.Multiply(ECKey.Secp256k1.N.Subtract(e));
-            var R = Rt1.Add(Rt2);
+			if(R.XCoord.ToBigInteger().CompareTo(sig.R) != 0 || BigInteger.Jacobi(R.YCoord.ToBigInteger(), PP) != 1 )
+				return false;
 
-            if(R.X.ToBigInteger().CompareTo(ri) != 0 || BigInteger.Jacobi(R.Y.ToBigInteger(), secP256K1_P) != 1 )
-                return false;
-
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
