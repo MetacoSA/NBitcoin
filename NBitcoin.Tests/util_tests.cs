da95dbcf31881c4da0165aad2774de8d2dc6eeeb
Lucas Ontivero <lucasontivero@gmail.com>
2018-07-16 12:51:18 -0300

    Prevent loosing money by parsing small values

--- NBitcoin.Tests/util_tests.cs
+++ NBitcoin.Tests/util_tests.cs
@@ -352,6 +352,9 @@ namespace NBitcoin.Tests
 
 				// Attempted 63 bit overflow should fail
 				Assert.False(Money.TryParse(prefix + "92233720368.54775808", out ret));
+
+				// Attempted parsing a very small value (loosing money)  should fail
+				Assert.False(Money.TryParse(prefix + "0.00000000000001", out ret));
 			}
 		}
 
