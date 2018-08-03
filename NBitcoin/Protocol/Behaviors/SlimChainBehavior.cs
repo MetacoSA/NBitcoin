1fddc208af394ae98646d144b7a5253b366675c6
NicolasDorier <nicolas.dorier@gmail.com>
2018-08-03 23:04:59 +0900

    fix build

--- NBitcoin/Protocol/Behaviors/SlimChainBehavior.cs
+++ NBitcoin/Protocol/Behaviors/SlimChainBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if !NOSOCKET
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
@@ -112,3 +113,4 @@ namespace NBitcoin.Protocol.Behaviors
 		}
 	}
 }
+#endif
