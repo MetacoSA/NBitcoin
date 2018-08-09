051d60171a9a403f6badb4eb58e45c88d4970132
Danystem <dan@dystem.io>
2018-08-09 11:43:44 +0100

    update Dystem configuration

--- NBitcoin.Altcoins/Dystem.cs
+++ NBitcoin.Altcoins/Dystem.cs
@@ -103,31 +103,34 @@ namespace NBitcoin.Altcoins
 					MajorityEnforceBlockUpgrade = 750,
 					MajorityRejectBlockOutdated = 950,
 					MajorityWindow = 1000,
-					BIP34Hash = new uint256("0x000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343"),
-					PowLimit = new Target(new uint256("0x000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343")),
-					MinimumChainWork = new uint256("0x000000000000000000000000000000000000000000000100a308553b4863b755"),
+					BIP34Hash = new uint256("00000f4fb42644a07735beea3647155995ab01cf49d05fdc082c08eb673433f9"),
+					PowLimit = new Target(0>>1),
+					MinimumChainWork = new uint256("000000000000000000000000000000000000000000000000010b219afffe4a8b"),
 					PowTargetTimespan = TimeSpan.FromSeconds(24 * 60 * 60),
-					PowTargetSpacing = TimeSpan.FromSeconds(2.5 * 60),
+					PowTargetSpacing = TimeSpan.FromSeconds(1 * 60),
 					PowAllowMinDifficultyBlocks = false,
 					CoinbaseMaturity = 30,
 					PowNoRetargeting = false,
 					RuleChangeActivationThreshold = 1916,
 					MinerConfirmationWindow = 2016,
 					ConsensusFactory = DystemConsensusFactory.Instance,
-					SupportSegwit = false
+					SupportSegwit = false,
+					CoinType = 222
 				})
 				.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] {30})
 				.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] {68})
 				.SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] {58})
 				.SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] {0x04, 0x88, 0xB2, 0x1E})
 				.SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] {0x04, 0x88, 0xAD, 0xE4})
-				
+
 				.SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("Dystem"))
 				.SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("Dystem"))
-				.SetMagic(0xBD6B0CBF)
+				
+				.SetMagic(0x3595a329)
 				.SetPort(16443)
 				.SetRPCPort(17200)
 				.SetMaxP2PVersion(70902)
+
 				.SetName("Dystem-main")
 				.AddAlias("Dystem-mainnet")
 				.AddDNSSeeds(new[]
@@ -153,17 +156,18 @@ namespace NBitcoin.Altcoins
 					MajorityRejectBlockOutdated = 75,
 					MajorityWindow = 100,
 					BIP34Hash = new uint256("0x0000047d24635e347be3aaaeb66c26be94901a2f962feccd4f95090191f208c1"),
-					PowLimit = new Target(new uint256("0x00000fffff000000000000000000000000000000000000000000000000000000")),
+					PowLimit = new Target(0>>1),
 					MinimumChainWork = new uint256("0x000000000000000000000000000000000000000000000000000924e924a21715"),
 					PowTargetTimespan = TimeSpan.FromSeconds(24 * 60 * 60),
-					PowTargetSpacing = TimeSpan.FromSeconds(2.5 * 60),
+					PowTargetSpacing = TimeSpan.FromSeconds(1 * 60),
 					PowAllowMinDifficultyBlocks = true,
 					CoinbaseMaturity = 30,
 					PowNoRetargeting = false,
 					RuleChangeActivationThreshold = 1512,
 					MinerConfirmationWindow = 2016,
 					ConsensusFactory = DystemConsensusFactory.Instance,
-					SupportSegwit = false
+					SupportSegwit = false,
+					CoinType =1
 				})
 				.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] {30})
 				.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] {68})
@@ -172,12 +176,15 @@ namespace NBitcoin.Altcoins
 				.SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] {0x04, 0x88, 0xAD, 0xE4})
 				.SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("tDystem"))
 				.SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("tDystem"))
-				.SetMagic(0xFFCAE2CE)
-				.SetPort(16443)
+				
+				.SetMagic(0x1d322dc2)
+				
+				.SetPort(65444)
 				.SetRPCPort(17200)
 				.SetMaxP2PVersion(70902)
-				.SetName("Dystem-test")
-				.AddAlias("Dystem-testnet")
+				
+				.SetName("dystem-test")
+				.AddAlias("dystem-testnet")
 				.AddDNSSeeds(new[]
 				{
 					new DNSSeedData("seed.dystem.io", "seed.dystem.io"),
@@ -201,12 +208,12 @@ namespace NBitcoin.Altcoins
 					MajorityRejectBlockOutdated = 950,
 					MajorityWindow = 1000,
 					BIP34Hash = new uint256("0x000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343"),
-					PowLimit = new Target(new uint256("0x000007d91d1254d60e2dd1ae580383070a4ddffa4c64c2eeb4a2f9ecc0414343")),
+					PowLimit = new Target(0>>1),
 					MinimumChainWork = new uint256("0x000000000000000000000000000000000000000000000100a308553b4863b755"),
 					PowTargetTimespan = TimeSpan.FromSeconds(24 * 60 * 60),
-					PowTargetSpacing = TimeSpan.FromSeconds(2.5 * 60),
+					PowTargetSpacing = TimeSpan.FromSeconds(1 * 60),
 					PowAllowMinDifficultyBlocks = false,
-					CoinbaseMaturity = 30,
+					CoinbaseMaturity = 10,
 					PowNoRetargeting = false,
 					RuleChangeActivationThreshold = 1916,
 					MinerConfirmationWindow = 2016,
@@ -216,16 +223,21 @@ namespace NBitcoin.Altcoins
 				.SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, new byte[] {30})
 				.SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, new byte[] {68})
 				.SetBase58Bytes(Base58Type.SECRET_KEY, new byte[] {58})
+
 				.SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, new byte[] {0x04, 0x88, 0xB2, 0x1E})
 				.SetBase58Bytes(Base58Type.EXT_SECRET_KEY, new byte[] {0x04, 0x88, 0xAD, 0xE4})
+				
 				.SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("tDystem"))
 				.SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("tDystem"))
-				.SetMagic(0xDCB7C1FC)
-				.SetPort(16443)
+				
+				.SetMagic(0x7d322ba2)
+				
+				.SetPort(65445)
 				.SetRPCPort(17200)
 				.SetMaxP2PVersion(70902)
-				.SetName("Dystem-reg")
-				.AddAlias("Dystem-regtest")
+
+				.SetName("dystem-reg")
+				.AddAlias("dystem-regtest")
 				.AddDNSSeeds(new DNSSeedData[0])
 				.AddSeeds(new NetworkAddress[0])
 				.SetGenesis("01000000000000000000000000000000000000000000000000000000000000000000000027cc0d8f6a20e41f445b1045d1c73ba4b068ee60b5fd4aa34027cbbe5c2e161e434fdb5af0ff0f1e4a6c51010101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff6704ffff001d01044c5e4120736c69702d75702062792073757065726d61726b6574204173646120696e20616e206f6e6c696e65206f7264657220736177206120776f6d616e206368617267656420c2a339333020666f7220612073696e676c652062616e616e61ffffffff010000000000000000434104575f641084f76b9e94aae509ce78f6213ee4855d5c245b76d931fa190a1b453edf3ecf2b28288a338ac186d07eedc6d99256838cb57322406edc697f239a0a6eac00000000");
