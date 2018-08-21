165f030b18cddbd162e49ec58593158f768d28e0
kuaidaoyanglang <1042744766@qq.com>
2018-08-21 10:57:42 +0800

    create a demo progect

---
+++ NBitcoin.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using NBitcoin.RPC;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace NBitcoin.Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RPCClient client = new RPCClient(new NetworkCredential("amo2017", "amo123456"), "127.0.0.1:8332", Network.Main);
+			Console.WriteLine(JsonConvert.SerializeObject(client.ListAccounts()));
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
