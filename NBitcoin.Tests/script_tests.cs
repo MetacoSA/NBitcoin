badddfd0c76b140ea7b7a68408125f6e23a1a531
Lucas Ontivero <lontivero@users.noreply.github.com>
2018-08-24 06:30:02 -0300

    Download bitcoin binaries from bitcoincore.org (#513)
    * Use bitcoincore.org instead of bitcoin.org
    
    Download bitcoin binary from bitcoincore.org instead of
    bitcoin.org website. Also, use the bitcoin client version 16.2.
    
    * Fix compilation warning
    
    * Restore deleted node 0.16.0 for testing

--- NBitcoin.Tests/script_tests.cs
+++ NBitcoin.Tests/script_tests.cs
@@ -391,7 +391,7 @@ namespace NBitcoin.Tests
 		private void EnsureHasLibConsensus()
 		{
 #if !NOCONSENSUSLIB
-			var bitcoinPath = NodeBuilder.EnsureDownloaded(NodeDownloadData.Bitcoin.v0_16_0);
+			var bitcoinPath = NodeBuilder.EnsureDownloaded(NodeDownloadData.Bitcoin.v0_16_2);
 
 			string libConsensusDll = null;
 			if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
