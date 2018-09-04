74193764d8cf3e39b1b595b0c50938fd185778a4
Andrew Camilleri <evilkukka@gmail.com>
2018-09-04 19:40:43 +0200

    community vs 2017

--- docs/serveDocs.sh
+++ docs/serveDocs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise"
+export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
 export VisualStudioVersion="15.0"
 
 docfx docfx.json --serve
