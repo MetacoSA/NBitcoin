e5f09d127e9448ffff2f421d1809550178c86125
joemphilips <joemphilips@gmail.com>
2018-12-07 16:33:11 +0900

    support bip67

--- NBitcoin/Utils.cs
+++ NBitcoin/Utils.cs
@@ -371,6 +371,24 @@ namespace NBitcoin
 	public class Utils
 	{
 
+		public class BytesComparer : Comparer<byte[]>
+		{
+			public override int Compare(byte[] x, byte[] y)
+			{
+				var len = Math.Min(x.Length, y.Length);
+				for (var i = 0; i < len; i++)
+				{
+					var c = x[i].CompareTo(y[i]);
+					if (c != 0)
+					{
+						return c;
+					}
+				}
+
+				return x.Length.CompareTo(y.Length);
+			}
+		}
+
 		internal static void SafeSet(ManualResetEvent ar)
 		{
 			try
