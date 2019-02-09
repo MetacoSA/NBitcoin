fab43bd1f52e255ba80104c3d8a335c0c0e31a10
lontivero <lucasontivero@gmail.com>
2019-02-09 16:41:29 -0300

    NBitcoin.Bench project

---
+++ NBitcoin.Bench/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace NBitcoin.Benck
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+		}
+	}
+}
