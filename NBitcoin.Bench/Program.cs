6644818f8eb363831c129cfdc7e3a3aa89f7d632
NicolasDorier <nicolas.dorier@gmail.com>
2019-02-18 17:57:55 +0900

    Benchmark of serialization and better benchmark for Golomb filter building.

--- NBitcoin.Bench/Program.cs
+++ NBitcoin.Bench/Program.cs
@@ -1,12 +1,20 @@
 ﻿using BenchmarkDotNet.Running;
 
-namespace NBitcoin.Benck
+namespace NBitcoin.Bench
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
 			BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+			//var s = new Serialization();
+			//s.Setup();
+			//s.DeserializeBigBlock();
+
+			//var s = new GolombRiceFilters();
+			//s.Setup();
+			//s.Build();
 		}
 	}
 }
---
+++ NBitcoin.Bench/Serialization.cs
@@ -0,0 +1,40 @@
+using System;
+using NBitcoin;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.IO;
+using NBitcoin.Bench;
+
+namespace NBitcoin.Bench
+{
+	[RPlotExporter, RankColumn]
+	public class Serialization
+	{
+		BlockSample Sample;
+		[GlobalSetup]
+		public void Setup()
+		{
+			Sample = new BlockSample();
+			Sample.Download();
+		}
+
+		[Benchmark]
+		public void SerializeBigBlock()
+		{
+			MemoryStream ms = new MemoryStream(Sample.BigBlockBytes.Length + 100);
+			var bstream = new BitcoinStream(ms, true);
+			Sample.BigBlock.ReadWrite(bstream);
+		}
+
+		[Benchmark]
+		public void DeserializeBigBlock()
+		{
+			MemoryStream ms = new MemoryStream(Sample.BigBlockBytes);
+			var bstream = new BitcoinStream(ms, false);
+			Block.CreateBlock(Network.Main).ReadWrite(bstream);
+		}
+	}
+}
