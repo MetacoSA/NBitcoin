c76955f82c72683a523e7dc58f6c1c9f0d465ff6
joemphilips <joemphilips@gmail.com>
2019-04-03 11:34:06 +0900

    pack Miniscript without editing .nuspec manually.

---
+++ pack.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -u
+
+readonly VERSION="1.0"
+if [[ "$(uname)" == 'Darwin' ]]; then
+  readonly SCRIPT_DIR_PATH=$(dirname $(greadlink -f $0))
+else
+  readonly SCRIPT_DIR_PATH=$(dirname $(readlink -f $0))
+fi
+
+cd $SCRIPT_DIR_PATH
+
+dotnet build NBitcoin.Miniscript -c Release
+dotnet pack NBitcoin -c Release
