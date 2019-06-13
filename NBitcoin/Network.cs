51f536a635e5f24fdbff7257f5dea54eae6e605c
Igor Guerrero <igorgue@protonmail.com>
2019-06-13 16:04:42 -0400

    Update Seed DNS on TestNet
    Source: https://github.com/bitcoin/bitcoin/blob/master/src/chainparams.cpp#L232-L236

--- NBitcoin/Network.cs
+++ NBitcoin/Network.cs
@@ -2201,7 +2201,9 @@ namespace NBitcoin
 #if !NOSOCKET
 			vFixedSeeds.Clear();
 			vSeeds.Clear();
-			vSeeds.Add(new DNSSeedData("bitcoin.petertodd.org", "testnet-seed.bitcoin.petertodd.org"));
+			vSeeds.Add(new DNSSeedData("bitcoin.jonasschnelli.ch", "testnet-seed.bitcoin.jonasschnelli.ch"));
+			vSeeds.Add(new DNSSeedData("tbtc.petertodd.org", "seed.tbtc.petertodd.org"));
+			vSeeds.Add(new DNSSeedData("bitcoin.sprovoost.nl", "seed.testnet.bitcoin.sprovoost.nl"));
 			vSeeds.Add(new DNSSeedData("bluematt.me", "testnet-seed.bluematt.me"));
 #endif
 
