6fb5350d157d50dcd9c03b5e650ce3d3a63b6b1c
NicolasDorier <nicolas.dorier@gmail.com>
2019-11-13 14:03:53 +0900

    Shift visual studio development to .net3.0

--- NBitcoin/PushNuget.ps1
+++ NBitcoin/PushNuget.ps1
@@ -1,5 +1,5 @@
 rm "bin\release\" -Recurse -Force
-dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg -p:Build30=true
+dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
 $package=(ls .\bin\Release\*.nupkg).FullName
 dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
 $ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '[^\.]*\.(\d+(\.\d+){1,3}).*', '$1')
