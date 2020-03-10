0191d93fc51e331969a8295aba3583275da96ddc
NicolasDorier <nicolas.dorier@gmail.com>
2020-03-10 15:22:44 +0900

    Fix version

--- NBitcoin.Secp256k1/PushNuget.ps1
+++ NBitcoin.Secp256k1/PushNuget.ps1
@@ -2,6 +2,6 @@ rm "bin\release\" -Recurse -Force
 dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
 $package=(ls .\bin\Release\*.nupkg).FullName
 dotnet nuget push $package --source "https://api.nuget.org/v3/index.json"
-$ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '[^\.]*\.(\d+(\.\d+){1,3}).*', '$1')
+$ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '.*(\d+\.\d+\.\d+)\.nupkg','$1')
 git tag -a "NBitcoin.Secp256k1/v$ver" -m "NBitcoin.Secp256k1/$ver"
 git push --tags
