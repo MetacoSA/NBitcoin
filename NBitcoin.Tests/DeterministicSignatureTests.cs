b450455579ae04e7830656721429caf1cab73ae9
Dan Gould <d@ngould.dev>
2020-03-28 22:50:29 -0400

    Test ECDSASignature.ToDER is Bip66 compatible (#836)
    * test ECDSASignature.ToDER is Bip66 compatible
    
    * swap TargetFrameworks' order
    
    * test #if HAS_SPAN in existing file
    
    * test if HAS_SPAN not if \!HAS_SPAN

--- NBitcoin.Tests/DeterministicSignatureTests.cs
+++ NBitcoin.Tests/DeterministicSignatureTests.cs
@@ -269,5 +269,37 @@ namespace NBitcoin.Tests
 				Assert.True(sig.ToDER().Length <= 70);
 			}
 		}
+
+#if HAS_SPAN
+		[Fact]
+		[Trait("UnitTest", "UnitTest")]
+		public void ECDSASignatureIsOnlyBip66DER()
+		{
+			var invalidBIP66DER = HexToByteArray("302402107777777777777777777777777777777702108777777777777777777777777777777701");
+			// Encodes:
+			// SEQUENCE {
+			//	 INTEGER 0x77777777777777777777777777777777,
+			//   INTEGER 0x87777777777777777777777777777777 // -1 exponent :(
+			// }
+			// BOOLEAN FALSE
+			var coercedSignature = new ECDSASignature(invalidBIP66DER);
+
+			var validBIP66DER = HexToByteArray("302502107777777777777777777777777777777702110087777777777777777777777777777777");
+			// Should coerce to this valid DER encoding:
+			// SEQUENCE {
+			//	 INTEGER 0x77777777777777777777777777777777,
+			//   INTEGER 0x0087777777777777777777777777777777 // +1 exponent :)
+			// }
+			Assert.True(coercedSignature.ToDER().SequenceEqual(validBIP66DER));
+		}
+
+		private static byte[] HexToByteArray(string hex)
+		{
+			byte[] bytes = new byte[hex.Length / 2];
+			for (int i = 0; i < hex.Length; i += 2)
+				bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+			return bytes;
+		}
+#endif
 	}
 }
