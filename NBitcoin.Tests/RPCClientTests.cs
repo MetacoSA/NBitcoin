4a8e2960ecd8b1eea3ea43195f98bfad222b9ed6
NicolasDorier <nicolas.dorier@gmail.com>
2020-04-03 15:23:44 +0900

    Fix test

--- NBitcoin.Tests/RPCClientTests.cs
+++ NBitcoin.Tests/RPCClientTests.cs
@@ -1187,7 +1187,7 @@ namespace NBitcoin.Tests
 
 				result = rpc.TestMempoolAccept(signedTx.SignedTransaction, false);
 				Assert.True(result.IsAllowed);
-				Assert.Equal((Protocol.RejectCode)0, result.RejectCode);
+				Assert.Equal(Protocol.RejectCode.INVALID, result.RejectCode);
 				Assert.Equal(string.Empty, result.RejectReason);
 			}
 		}
