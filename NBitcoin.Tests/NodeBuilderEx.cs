b9fe273b249bdc60a49afacf4f4191c5ef7900c1
Sotiris Blad <sotblad@monetaryunit.org>
2020-07-24 16:03:07 +0300

    Update NodeBuilderEx.cs

--- NBitcoin.Tests/NodeBuilderEx.cs
+++ NBitcoin.Tests/NodeBuilderEx.cs
@@ -61,6 +61,8 @@ namespace NBitcoin.Tests
 			//var builder = NodeBuilder.Create(NodeDownloadData.Argoneum.v1_4_1, Altcoins.Argoneum.Instance.Regtest, caller);
 
 			//var builder = NodeBuilder.Create(NodeDownloadData.Qtum.v0_18_3, Altcoins.Qtum.Instance.Regtest, caller);
+			
+			//var builder = NodeBuilder.Create(NodeDownloadData.MonetaryUnit.v2_1_6, Altcoins.MonetaryUnit.Instance.Regtest, caller);
 
 			//var builder = Create(NodeDownloadData.Bitcoin.v0_19_0_1, caller);
 
