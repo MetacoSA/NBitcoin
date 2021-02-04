e3cdfc4011ec1c834d03d5658bea5df127848478
Lucas Ontivero <lucasontivero@gmail.com>
2021-02-04 01:28:26 -0300

    getcfcheckpt bip 157 p2p message
    getcfcheckpt is used to request filter headers at evenly spaced intervals over a range of blocks. Clients may use filter hashes from getcfheaders to connect these checkpoints,

--- NBitcoin/Protocol/Payloads/GetCompactFiltersPayload.cs
+++ NBitcoin/Protocol/Payloads/GetCompactFiltersPayload.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace NBitcoin.Protocol
 {
@@ -85,5 +85,45 @@ namespace NBitcoin.Protocol
 		}
 
 		public GetCompactFilterHeadersPayload() { }
+	}
+
+	[Payload("getcfcheckpt")]
+	public class GetCompactFilterCheckPointPayload : Payload
+	{
+		private byte _FilterType;
+		private uint256 _StopHash;
+
+		public GetCompactFilterCheckPointPayload(FilterType filterType, uint256 stopHash)
+		{
+			if (filterType != FilterType.Basic)
+				throw new ArgumentException($"'{filterType}' is not a valid value.", nameof(filterType));
+			if (stopHash == null)
+				throw new ArgumentNullException(nameof(stopHash));
+
+			FilterType = filterType;
+			_StopHash = stopHash;
+		}
+
+		public GetCompactFilterCheckPointPayload() { }
+
+		public override void ReadWriteCore(BitcoinStream stream)
+		{
+			stream.ReadWrite(ref _FilterType);
+			stream.ReadWrite(ref _StopHash);
+		}
+
+		/// <summary>
+		/// Gets the Filter type for which headers are requested.
+		/// </summary>
+		public FilterType FilterType
+		{
+			get => (FilterType)_FilterType;
+			internal set => _FilterType = (byte)value;
+		}
+
+		/// <summary>
+		/// Gets the hash of the last block in the requested range.
+		/// </summary>
+		public uint256 StopHash => _StopHash;
 	}
 }
