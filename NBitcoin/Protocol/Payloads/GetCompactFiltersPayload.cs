6e47ab10b7d43ec07c57bf5cf94f5f79c05e68fc
Lucas Ontivero <lucasontivero@gmail.com>
2021-02-04 01:24:11 -0300

    Base class for compact filters queries
    Contains what is common to all compact filters
    queries:
    
    * FilterType
    * StartHeight
    * StopHash

--- NBitcoin/Protocol/Payloads/GetCompactFiltersPayload.cs
+++ NBitcoin/Protocol/Payloads/GetCompactFiltersPayload.cs
@@ -7,4 +7,55 @@ namespace NBitcoin.Protocol
 		Basic = (0x00),
 	}
 
+	/// <summary>
+	/// Represents the p2p message payload used for requesting a range of compact filter/headers.
+	/// </summary>
+	public abstract class CompactFiltersQueryPayload : Payload
+	{
+		private byte _FilterType = (byte)FilterType.Basic;
+		private uint _StartHeight = 0;
+		private uint256 _StopHash = new uint256();
+
+		/// <summary>
+		/// Gets the Filter type for which headers are requested.
+		/// </summary>
+		public FilterType FilterType
+		{
+			get => (FilterType)_FilterType;
+			internal set => _FilterType = (byte)value;
+		}
+
+		/// <summary>
+		/// Gets the height of the first block in the requested range.
+		/// </summary>
+		public uint StartHeight => _StartHeight;
+		
+		/// <summary>
+		/// Gets the hash of the last block in the requested range.
+		/// </summary>
+		public uint256 StopHash => _StopHash;
+		
+		protected CompactFiltersQueryPayload(FilterType filterType, uint startHeight, uint256 stopHash)
+		{
+			if (filterType != FilterType.Basic)
+				throw new ArgumentException($"'{filterType}' is not a valid value.", nameof(filterType));
+			if (stopHash == null)
+				throw new ArgumentNullException(nameof(stopHash));
+
+			FilterType = filterType;
+			_StartHeight = startHeight;
+			_StopHash = stopHash;
+		}
+
+		protected CompactFiltersQueryPayload() { }
+
+		#region IBitcoinSerializable Members
+		public override void ReadWriteCore(BitcoinStream stream)
+		{
+			stream.ReadWrite(ref _FilterType);
+			stream.ReadWrite(ref _StartHeight);
+			stream.ReadWrite(ref _StopHash);
+		}
+		#endregion
+	}
 }
