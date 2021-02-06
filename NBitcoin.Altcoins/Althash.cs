1f8159f711c360f10e7d3be4d1f1828e1926ff6e
Jaron Rosenau <jaron@rosenau.info>
2021-02-06 05:06:27 -0800

    fix id and remove MaxP2P

--- NBitcoin.Altcoins/Althash.cs
+++ NBitcoin.Altcoins/Althash.cs
@@ -179,8 +179,7 @@ namespace NBitcoin.Altcoins
 			.SetMagic(0x1f2e3d4c)
 			.SetPort(4888)
 			.SetRPCPort(4889)
-			.SetMaxP2PVersion(70018)
-			.SetName("althash-main")
+			.SetName("main")
 			.AddAlias("althash-mainnet")
 			.AddDNSSeeds(new[]{
 				new DNSSeedData("seed1.htmlcoin.com", "seed2.htmlcoin.com"),
@@ -219,8 +218,7 @@ namespace NBitcoin.Altcoins
 			.SetMagic(0x2f3e4d5c)
 			.SetPort(14888)
 			.SetRPCPort(14889)
-			.SetMaxP2PVersion(70017)
-			.SetName("althash-test")
+			.SetName("test")
 			.AddAlias("althash-testnet")
 			.AddDNSSeeds(new[]{
 				new DNSSeedData("testnet-seed1.htmlcoin.com", "testnet-seed2.htmlcoin.com"),
@@ -260,8 +258,7 @@ namespace NBitcoin.Altcoins
 			.SetMagic(0x3f4e5d6c)
 			.SetPort(24888)
 			.SetRPCPort(14889)
-			.SetMaxP2PVersion(70017)
-			.SetName("althash-reg")
+			.SetName("regtest")
 			.AddAlias("althash-regtest")
 			.AddSeeds(new NetworkAddress[0])
 			// Incorrect, using mainnet for now
