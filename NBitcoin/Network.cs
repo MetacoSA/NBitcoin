b9ddafa8bbea3b98ef4424109e82ec9ac2e276f5
Yahia Chiheb <chihebyahia@gmail.com>
2022-03-20 20:52:19 +0100

    Remove unused methods

--- NBitcoin/Network.cs
+++ NBitcoin/Network.cs
@@ -2369,33 +2369,6 @@ namespace NBitcoin
 			}
 		}
 #endif
-		private Block CreateGenesisBlock(uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
-		{
-			string pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
-			Script genesisOutputScript = new Script(Op.GetPushOp(Encoders.Hex.DecodeData("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f")), OpcodeType.OP_CHECKSIG);
-			return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
-		}
-
-		private Block CreateGenesisBlock(string pszTimestamp, Script genesisOutputScript, uint nTime, uint nNonce, uint nBits, int nVersion, Money genesisReward)
-		{
-			Transaction txNew = Consensus.ConsensusFactory.CreateTransaction();
-			txNew.Version = 1;
-			txNew.Inputs.Add(scriptSig: new Script(Op.GetPushOp(486604799), new Op()
-			{
-				Code = (OpcodeType)0x1,
-				PushData = new[] { (byte)4 }
-			}, Op.GetPushOp(Encoders.ASCII.DecodeData(pszTimestamp))));
-			txNew.Outputs.Add(genesisReward, genesisOutputScript);
-			Block genesis = Consensus.ConsensusFactory.CreateBlock();
-			genesis.Header.BlockTime = Utils.UnixTimeToDateTime(nTime);
-			genesis.Header.Bits = nBits;
-			genesis.Header.Nonce = nNonce;
-			genesis.Header.Version = nVersion;
-			genesis.Transactions.Add(txNew);
-			genesis.Header.HashPrevBlock = uint256.Zero;
-			genesis.UpdateMerkleRoot();
-			return genesis;
-		}
 
 		private static void assert(bool v)
 		{
