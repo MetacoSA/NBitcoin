1c36b2958d30964e1510bcedebb4309467e00073
Stephen Heindel <sheindel@logikos.com>
2022-05-10 23:45:54 -0400

    Added URL Safe Base64 Data Encoder

---
+++ NBitcoin/DataEncoders/Base64UrlSafeEncoder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NBitcoin.DataEncoders
+{
+	public class Base64UrlSafeEncoder : DataEncoder
+	{
+		static readonly char[] padding = { '=' };
+		public override byte[] DecodeData(string encoded)
+		{
+			return Convert.FromBase64String(encoded.TrimEnd(padding).Replace('-', '+').Replace('_', '/'));
+		}
+
+		public override string EncodeData(byte[] data, int offset, int count)
+		{
+			return Convert.ToBase64String(data, offset, count).Replace("+", "-").Replace("/", "_");
+		}
+	}
+}
