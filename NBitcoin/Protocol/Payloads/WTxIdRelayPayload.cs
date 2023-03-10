9fa1adc07b0d921a2ec4b5780898d804c437a2d8
Vladimir Jerabek <jerab.vl@gmail.com>
2023-03-10 05:30:25 +0100

    feat: add WTxIdRelayPayload (#1161)

---
+++ NBitcoin/Protocol/Payloads/WTxIdRelayPayload.cs
@@ -0,0 +1,10 @@
+namespace NBitcoin.Protocol
+{
+	/// <summary>
+	///	Signaling message about WTXID support (see BIP339)
+	/// </summary>
+	[Payload("wtxidrelay")]
+	public class WTxIdRelayPayload : Payload
+	{
+	}
+}
