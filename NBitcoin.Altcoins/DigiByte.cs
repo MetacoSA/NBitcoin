7490ba414376f5c24cd75715fb7d3bfa730730e2
Owen Ashurst <owen.ashurst@googlemail.com>
2025-05-10 17:55:50 +0100

    Update DigiByte.cs

--- NBitcoin.Altcoins/DigiByte.cs
+++ NBitcoin.Altcoins/DigiByte.cs
@@ -6,6 +6,7 @@ namespace NBitcoin.Altcoins
     {
         public static DigiByte Instance { get; } = new();
         public override string CryptoCode => "DGB";
+
         private DigiByte() { }
 
         protected override NetworkBuilder CreateMainnet()
@@ -44,35 +45,34 @@ namespace NBitcoin.Altcoins
 
         protected override NetworkBuilder CreateTestnet()
         {
-            var builder = new NetworkBuilder();
+	        var builder = new NetworkBuilder()
+		        .SetName("dgb-test")
+		        .AddAlias("digibyte-testnet")
+		        .AddAlias("digibyte-test")
+		        .SetConsensus(new Consensus
+		        {
+			        SubsidyHalvingInterval = 210000,
+			        MajorityEnforceBlockUpgrade = 750,
+			        MajorityRejectBlockOutdated = 950,
+			        MajorityWindow = 1000
+		        })
+		        .SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, [0x7E])  // tD
+		        .SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, [0x8C]) // tS
+		        .SetBase58Bytes(Base58Type.SECRET_KEY, [0xFE])      // tWIF
+		        .SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, [0x04, 0x35, 0x87, 0xCF])
+		        .SetBase58Bytes(Base58Type.EXT_SECRET_KEY, [0x04, 0x35, 0x83, 0x94])
+		        .SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("dgbt"))
+		        .SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("dgbt"))
+		        .SetMagic(0xDDB6C3FA)
+		        .SetPort(12026)
+		        .SetRPCPort(14023)
+		        .SetGenesis("0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c")
+		        .AddDNSSeeds([
+			        new DNSSeedData("seed.testnet-1.us.digibyteservers.io", "seed.testnet-1.us.digibyteservers.io"),
+			        new DNSSeedData("seed.testnetexplorer.digibyteservers.io", "seed.testnetexplorer.digibyteservers.io")
+		        ]);
 
-            builder.SetName("dgb-test")
-                   .AddAlias("dgb-testnet")
-                   .AddAlias("digibyte-test")
-                   .SetConsensus(new Consensus
-                   {
-                       SubsidyHalvingInterval       = 210000,
-                       MajorityEnforceBlockUpgrade  = 750,
-                       MajorityRejectBlockOutdated  = 950,
-                       MajorityWindow               = 1000,
-                   })
-                   .SetBase58Bytes(Base58Type.PUBKEY_ADDRESS, [126])
-                   .SetBase58Bytes(Base58Type.SCRIPT_ADDRESS, [140])
-                   .SetBase58Bytes(Base58Type.SECRET_KEY, [254])
-                   .SetBase58Bytes(Base58Type.EXT_PUBLIC_KEY, [0x04, 0x35, 0x87, 0xCF])
-                   .SetBase58Bytes(Base58Type.EXT_SECRET_KEY, [0x04, 0x35, 0x83, 0x94])
-                   .SetBech32(Bech32Type.WITNESS_PUBKEY_ADDRESS, Encoders.Bech32("dgbt"))
-                   .SetBech32(Bech32Type.WITNESS_SCRIPT_ADDRESS, Encoders.Bech32("dgbt"))
-                   .SetMagic(0xDDBDC8FD)
-                   .SetPort(12026)
-                   .SetRPCPort(14023)
-                   .SetGenesis("0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c")
-                   .AddDNSSeeds([
-	                   new DNSSeedData("seed.testnet-1.us.digibyteservers.io","seed.testnet-1.us.digibyteservers.io"),
-                       new DNSSeedData("seed.testnetexplorer.digibyteservers.io","seed.testnetexplorer.digibyteservers.io")
-                   ]);
-
-            return builder;
+	        return builder;
         }
 
         protected override NetworkBuilder CreateRegtest()
