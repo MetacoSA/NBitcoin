be83891b4321753f2391fdc9d3985878fee63dd2
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-04-02 10:36:47 +0900

    Fix pushnuget script

--- NBitcoin.Altcoins/PushNuget.sh
+++ NBitcoin.Altcoins/PushNuget.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 rm -rf "bin/release/"
 dotnet pack --configuration Release --include-symbols -p:SymbolPackageFormat=snupkg
 package=$(find ./bin/Release -name "*.nupkg" -type f | head -n 1)
-dotnet nuget push "$package" --source "https://api.nuget.org/v3/index.json"
+dotnet nuget push "$package" --source "https://api.nuget.org/v3/index.json" --api-key "$NUGET_API_KEY"
 ver=$(basename "$package" | sed -E 's/[^.]*\.[^.]*\.([0-9]+(\.[0-9]+){1,3}).*/\1/')
 git tag -a "NBitcoin.Altcoins/v$ver" -m "NBitcoin.Altcoins/$ver"
 git push --tags
