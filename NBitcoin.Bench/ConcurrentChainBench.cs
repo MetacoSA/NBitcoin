920bb363b1b0f317ea3988e95b34ca9f0d8409ab
Carti <carti_it@proton.me>
2026-06-14 11:24:43 +0200

    Add ConcurrentChainBench

---
+++ NBitcoin.Bench/ConcurrentChainBench.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.IO;
+
+namespace NBitcoin.Bench;
+
+public class ConcurrentChainBench
+{
+	byte[] Bytes; 
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		var appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "WalletWasabi", "Client", "BitcoinP2pNetwork", "BlockHeadersMain.dat");
+		Bytes = File.ReadAllBytes(appDataPath);
+	}
+
+	[Benchmark]
+	public void WriteToString()
+	{
+		var chain = new ConcurrentChain(Bytes, Network.Main.Consensus.ConsensusFactory);
+	}
+}
