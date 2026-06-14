8bc2955c142ca45d780c41f70e37362fce42a053
Carti <carti_it@proton.me>
2026-06-14 13:10:19 +0200

    Add `HashBench`

---
+++ NBitcoin.Bench/HashBench.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using NBitcoin.Crypto;
+using NBitcoin.DataEncoders;
+
+namespace NBitcoin.Bench;
+
+[MemoryDiagnoser]
+public class HashBench
+{
+	private byte[] bytes;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		bytes = Encoders.Hex.DecodeData("d54994ece1d11b19785c7248868696250ab195605b469632b7bd68130e880c9a");
+	}
+
+	[Benchmark]
+	public void SHA1()
+	{
+		Hashes.SHA1(bytes, 0, bytes.Length);
+	}
+
+	[Benchmark]
+	public void SHA256()
+	{
+		Hashes.SHA256(bytes);
+	}
+
+	[Benchmark]
+	public void SHA512()
+	{
+		Hashes.SHA512(bytes);
+	}
+}
